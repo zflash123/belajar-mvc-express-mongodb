5db13d670d53da5d550434f9d7782ec38f600846
Zaed Abdullah <z4ed.thalib123@gmail.com>
2023-05-20 13:58:48 +0700

    add POST request for registering user

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,17 +1,61 @@
 //js
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 //For Register Page
 const registerView = (req, res) => {
     res.render('register', {
-
     })
 }
 
-//For View
+//For Login View
 const loginView = (req, res) => {
     res.render('login', {
-
     })
+}
+
+// POST request that handles register
+const registerUser = (req, res) => {
+    const {name, email, password, location, created_at} = req.body
+    if(!name || !email || !password || !location || !created_at){
+        console.log('There is field that has empty value')
+    }
+    //Confirm Passwords
+    if (password !== confirm){
+        console.log('Password must match')
+    } else {
+        //Validation
+        User.findOne({ email: email }).then((user) => {
+            if(user){
+                console.log('email exists')
+                res.render('register', {
+                    name,
+                    email,
+                    password,
+                    confirm,
+                })
+            } else {
+                //Validation
+                const newUser = new User({
+                    name,
+                    email,
+                    location,
+                    password,
+                })
+                //Password Hashing
+                bcrypt.genSalt(10, (err, salt) => 
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            .then(res.redirect('/login'))
+                            .catch((err) => console.log(err));
+                    })
+                );
+            }
+        })
+    }
 }
 
 module.exports = {
